871bf7c31ba0c6a626ebd92bf13767d071c0c6ff
pallen19 <paulanceallen@live.com>
2022-08-29 17:54:34 -0400

    Add files via upload

---
+++ Assignments/Assignment1/Assignment1.java
@@ -0,0 +1,15 @@
+// Name: <Paul Allen>
+// Class: CS 3305/W04
+// Term: Fall 2022
+// Instructor: Sharon Perry
+// Assignment: 1 Hello World
+package hello;
+public class Assignment1 {
+
+	public static void main(String[] Args) {
+		
+		System.out.println("Hello World!");
+		
+		
+	}
+}
